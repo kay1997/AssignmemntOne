80aa7974acdad4d3e1b564a14c84be5f2b29d85b
Morgan1623 <47596202+Morgan1623@users.noreply.github.com>
2019-02-13 13:58:04 +0200

    Add files via upload

---
+++ CalcAssignment1/src/main/java/za/ac/cput/CalcAssignment1/Calculate.java
@@ -0,0 +1,23 @@
+package za.ac.cput.CalcAssignment1;
+
+public class Calculate
+{
+
+        public int addition(int a, int b)
+        {
+            return a +b;
+        }
+
+    public int subtraction (int a, int b)
+    {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+
+
+}
---
+++ CalcAssignment1/src/test/java/CalculateTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import za.ac.cput.CalcAssignment1.Calculate;
+import org.junit.Test;
+
+public class CalculateTest
+{
+    @Test
+    public void addition()
+    {
+        Calculate calc = new Calculate();
+        Assert.assertEquals(10, calc.addition(5,5));
+
+    }
+
+    @Test
+    public void subtraction()
+    {
+        Calculate calc = new Calculate();
+        Assert.assertEquals(0, calc.subtraction(5,5));
+    }
+
+    @Test
+    public void multiplication()
+    {
+        Calculate calc = new Calculate();
+        Assert.assertEquals(25, calc.multiplication(5,5));
+    }
+}
